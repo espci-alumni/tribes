8d4ebfe0450bd09e35d539af501b8dd843b80ba4
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-01-08 17:52:04 +0000

    * tribes/contact, réorganisation du bloc-note autour de secretariat

--- produits/tribes/contact/class/agent/user/secretariat.php
+++ produits/tribes/contact/class/agent/user/secretariat.php
@@ -1,17 +1,25 @@
 <?php
 
-class extends agent_user_blocnote
+class extends agent_pForm
 {
-	public $get = array('__1__:i:1');
-	protected $f;
+	protected $contact_id, $form;
+
+	function compose($o)
+	{
+		isset($this->contact_id) || $this->contact_id = $this->connected_id;
+
+		return parent::compose($o);
+	}
 
 	function composeForm($o, $f, $send)
 	{
-		$this->contact_id = $this->get->__1__;
-		$f->add('textarea', 'note');
-		
-		$send->attach('note', '', '');
+		$this->form = $f;
 
+		return $this->composeBlocnote($o, $f, $send);
+	}
+
+	function composeBlocnote($o, $f, $send)
+	{
 		$sql = "SELECT h.*, prenom_usuel AS origine_prenom, nom_usuel As origine_nom, login AS origine_login, origine_contact_id, date_contact
 				FROM contact_historique h
 					JOIN contact_contact c ON c.contact_id=h.origine_contact_id
@@ -26,21 +34,12 @@ class extends agent_user_blocnote
 
 	function filterRow($o)
 	{
-		$this->f = new pForm($o);
-		$o = parent::filterRow($o);
-		$o->f_del = new pForm_check($this->f, 'f_del',
-				array(
-					'item' => array($o->historique_id => 'suppression'),
-					'default' => '',
-					'multiple' => true
-				)
-			);
-		
+		$sql = unserialize($o->details);
+
+		$o->note = $sql['note'];
+
+		unset($o->details);
+
 		return $o;
-	}
-	function save($data)
-	{
-		E($data);
-		return '';
 	}
 }
