9ea86c4cd134b30087809de4df26cb63c0adb51f
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-08-27 20:29:14 +0200

    * définition du champ promotion par la base SQL

--- produits/tribes/diplome/class/agent/registration.php
+++ produits/tribes/diplome/class/agent/registration.php
@@ -12,7 +12,39 @@ class extends self
 
 	protected function composeDiplome($o, $f, $send)
 	{
-		$f->add('text', 'promotion');
+		$promotions = array();
+
+		$sql = "(SELECT COALESCE((SELECT value FROM item_lists WHERE type='promotion/limits' LIMIT 1),'') AS promotion)
+			UNION ALL (SELECT value FROM item_lists WHERE type='promotion/label' ORDER BY sort_key)";
+
+		$result = DB()->query($sql);
+
+		if ($row = $result->fetchRow())
+		{
+			while ($sql = $result->fetchRow())
+			{
+				$promotions[$sql->promotion] = $sql->promotion;
+			}
+
+			$row = explode(':', $row->promotion, 2);
+
+			if (2 === count($row))
+			{
+				foreach (range($row[0], $row[1]) as $row)
+				{
+					$promotions[$row] = $row;
+				}
+			}
+		}
+
+		if ($promotions)
+		{
+			$f->add('select', 'promotion', array('item' => $promotions));
+		}
+		else
+		{
+			$f->add('text', 'promotion');
+		}
 
 		$send->attach('promotion', 'Veuillez renseignez votre promotion', '');
 
