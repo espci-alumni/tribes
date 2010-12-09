1336f1814c2fb0e8678b80f751fb981a6af82f76
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-12-09 13:38:06 +0100

    * doc et fix pour paiement en ligne Atis SIPS

--- produits/tribes/cotisation/class/agent/tpe/atossips/request.php
+++ produits/tribes/cotisation/class/agent/tpe/atossips/request.php
@@ -9,42 +9,52 @@ class extends agent
 
 	$parameters = array(
 		// XXX Deux principaux paramètres à ajuster
-		// 'merchant_id'      => '012345678901234',
-		'pathfile'         => 'data/atossips/pathfile',
+		// 'merchant_id' => '012345678901234',
+		'pathfile'     => 'data/atossips/pathfile',
 
-		// 'merchant_country' => 'fr',
-		// 'currency_code'    => '978', // EUR
+		// --- Géré automatiquement ---
+		// 'transaction_id'       => '123456',     // généré par le binaire
+		// 'amount'               => '123456',     // en centimes
+		// 'normal_return_url'    => 'http://...', // RETURN_URL
+		// 'cancel_return_url'    => 'http://...', // CANCEL_URL
+		// 'automatic_return_url' => 'http://...', // AUTO_RESPONSE_URL
+		// 'customer_id'          => '',           // 19 pristine req. to resp. data (forbidden chars: [|;:"])
+		// 'customer_email'       => '',
 
-		// Si aucun transaction_id n'est affecté, request en génère
-		// un automatiquement à partir de heure/minutes/secondes
-		// Référez vous au Guide du Programmeur pour
-		// les réserves émises sur cette fonctionnalité
-		//
-		// 'transaction_id' => '123456',
+		// --- Valeurs par défaut du fichier parmcom.default ---
+		// 'bgcolor'           => 'ffffff',
+		// 'block_align'       => 'center',
+		// 'block_order'       => '1,2,3,4,5,6,7,8',
+		// 'condition'         => 'SSL',
+		// 'currency_code'     => '978', // CURRENCY | 978 pour €
+		// 'header_flag'       => 'no',
+		// 'language'          => 'fr',
+		// 'merchant_country'  => 'fr',
+		// 'payment_means'     => 'CB,1,VISA,1,MASTERCARD,1',
+		// 'target'            => '_top',
+		// 'textcolor'         => '000000',
 
+		// --- Autres paramètres (transaction) ---
+		// 'capture_day'         => '', // nombre de jours avant transaction
+		// 'capture_mode'        => '', // mode d’envoi en banque de la transaction.
+		// 'data'                => '', // 2048 chars for advanced parameters
 
-		// Affectation dynamique des autres paramètres
-		// Les valeurs proposées ne sont que des exemples
-		// Les champs et leur utilisation sont expliqués dans le Dictionnaire des données
-		//
-		// 'language'            => 'fr',
-		// 'payment_means'       => 'CB,2,VISA,2,MASTERCARD,2',
-		// 'header_flag'         => 'no',
-		// 'capture_day'         => '',
-		// 'capture_mode'        => '',
-		// 'bgcolor'             => '',
-		// 'block_align'         => '',
-		// 'block_order'         => '',
-		// 'textcolor'           => '',
-		// 'receipt_complement'  => '',
-		// 'caddie'              => 'mon_caddie',
-		// 'customer_id'         => '',
-		// 'customer_email'      => '',
-		// 'customer_ip_address' => '',
-		// 'data'                => '',
-		// 'return_context'      => '',
-		// 'target'              => '',
-		// 'order_id'            => '',
+		// --- Autres paramètres (session) ---
+		// 'customer_ip_address' => '', //   19 pristine req. to resp. data
+		// 'order_id'            => '', //   32 pristine req. to resp. data (forbidden chars: [|;:"])
+		// 'return_context'      => '', //  256 pristine req. to resp. data (forbidden chars: [|;:"])
+		// 'caddie'              => '', // 2048 pristine req. to resp. data
+
+		// --- Autres paramètres (pages sur le serveur SIPS) ---
+		// 'advert'              => '', // ADVERT      | bannière centrée
+		// 'logo_id'             => '', // LOGO        | image alignée à gauche
+		// 'logo_id2'            => '', // LOGO2       | image alignée à droite
+		// 'background_id'       => '', // BACKGROUND  | image de fond
+		// 'submit_logo'         => '', // SUBMIT_LOGO | image du bouton Valider
+		// 'cancel_return_logo'  => '', // CANCEL_LOGO | image du bouton Annuler
+		// 'normal_return_logo'  => '', // RETURN_LOGO | image du bouton Retour
+		// 'receipt_complement'  => '', // RECEIPT     | 3072 HTML data affichées sur l'accusé de réception
+		// 'templatefile'        => '', // TEMPLATE    | fichier HTML de personnalisation des pages
 	);
 
 	protected $requiredAuth = false;
