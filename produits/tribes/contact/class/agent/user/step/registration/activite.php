971aef7664a903f02c5f528b32a79bc2629a0fb3
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-03-08 19:41:48 +0100

    * Tribes: contact.statut_activite à la place de activite.statut à l'inscription

--- produits/tribes/contact/class/agent/user/step/registration/activite.php
+++ produits/tribes/contact/class/agent/user/step/registration/activite.php
@@ -9,11 +9,7 @@ class extends agent_user_step_registration
 
 	protected function composeActivite($o, $f, $send, $new = false)
 	{
-		if (isset($_POST['f_statut_activite_1']))
-		{
-			$f->add('text', 'statut_activite', array(), false)->setValue($_POST['f_statut_activite_1']);
-			$send->attach('statut_activite', '', '');
-		}
+		$o = parent::composeActivite($o, $f, $send, true);
 
 		$this->activites = new loop_edit_contact_activiteStep($f, $send);
 
--- produits/tribes/contact/class/loop/edit/contact/activiteStep.php
+++ produits/tribes/contact/class/loop/edit/contact/activiteStep.php
@@ -10,6 +10,8 @@ class extends loop_edit_contact_activite
 
 		$default = array(
 			'activite_id' => 0,
+			'statut'      => $f->getElement('statut_activite')->getValue(),
+			'hide_statut' => 1,
 		);
 
 		loop_edit::__construct($f, new loop_array(array($default), 'filter_rawArray'));
