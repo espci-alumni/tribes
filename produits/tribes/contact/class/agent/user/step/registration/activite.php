ea40dc12fdaaa2fa76977b3feef2da4304e5f8d7
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-03-14 20:56:36 +0100

    * tribes: champs "ville" à l'étape registration/activite

--- produits/tribes/contact/class/agent/user/step/registration/activite.php
+++ produits/tribes/contact/class/agent/user/step/registration/activite.php
@@ -18,6 +18,26 @@ class extends agent_user_step_registration
 
 	protected function save($data)
 	{
+		while ($b = $this->activites->loop())
+		{
+			$rpos = $b->f_ville->getDbValue();
+			$data = array(
+				'adresse'    => '',
+				'ville'      => $rpos,
+				'is_shared'  => 1,
+				'contact_id' => $this->contact_id,
+
+			);
+
+			if (false !== $rpos = strrpos($rpos, ','))
+			{
+				$data['pays']  = trim(substr($data['ville'], $rpos+1));
+				$data['ville'] = trim(substr($data['ville'], 0, $rpos));
+			}
+
+			$this->adresse->save($data, null, $_POST[$b->f_adresse_id->getName()]);
+		}
+
 		$this->saveActivite($data);
 
 		return parent::save($data);
--- produits/tribes/contact/class/loop/edit/contact/activiteStep.php
+++ produits/tribes/contact/class/loop/edit/contact/activiteStep.php
@@ -5,7 +5,6 @@ class extends loop_edit_contact_activite
 	function __construct($f, $send)
 	{
 		$this->allowAddDel = false;
-		$this->editAdresse = false;
 		$this->send = $send;
 
 		$default = array(
@@ -15,5 +14,12 @@ class extends loop_edit_contact_activite
 		);
 
 		loop_edit::__construct($f, new loop_array(array($default), 'filter_rawArray'));
+	}
+
+	function populateForm($a, $data, $counter)
+	{
+		parent::populateForm($a, $data, $counter);
+		$this->form->add('city', 'ville', array('isdata' => false));
+		$this->send->attach('ville', $this->adminMode ? '' : "Veuillez choisir ou ajouter une ville", '');
 	}
 }
