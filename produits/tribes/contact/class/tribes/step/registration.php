25dfa4bd6b27f83626021f47ff3611a73759593f
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-03-21 15:49:14 +0100

    * update clients/minerve

--- produits/tribes/contact/class/tribes/step/registration.php
+++ produits/tribes/contact/class/tribes/step/registration.php
@@ -3,7 +3,7 @@
 class extends tribes_step
 {
 	protected $steps = array(
-		'registration/contact'   => "État civil / Cursus",
+		'registration/contact'   => "État civil / Formation",
 		'registration/adresse'   => "Adresse de correspondance",
 		'registration/activite'  => "Activité professionnelle",
 		'registration/bienvenue' => "Message de bienvenue",
