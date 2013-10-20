5888e03e239299656f7104fd0f00c4189bd0bd82
Nicolas Grekas <nicolas.grekas@gmail.com>
2013-10-20 16:58:01 +0000

    fix

--- produits/tribes/contact/class/agent/admin/export/excel.php
+++ produits/tribes/contact/class/agent/admin/export/excel.php
@@ -13,8 +13,8 @@ class agent_admin_export_excel extends agent_admin_export
 
         $sheet = $this->sheet = $book->addWorksheet('Contacts');
         $sheet->setInputEncoding('UTF-8');
-		$this->head = $book->addFormat();
-		$this->head->setBold();
+        $this->head = $book->addFormat();
+        $this->head->setBold();
 
         $o = parent::compose($o);
 
@@ -27,21 +27,21 @@ class agent_admin_export_excel extends agent_admin_export
     {
         if (0 === $count)
         {
-			$h = array();
+            $h = array();
             $col = 0;
             foreach ($row as $k => $v)
-			{
-				$this->sheet->write(0, $h[$k] = $col++, $k, $this->head);
-			}
+            {
+                $this->sheet->write(0, $h[$k] = $col++, $k, $this->head);
+            }
 
-            $this->sheet->freezePanes(array(1, 0));
-			$this->head = $h;
+            $this->sheet->freezePanes(array(1, 0, 1, 0));
+            $this->head = $h;
         }
 
         foreach ($row as $k => $v)
         {
-			if (isset($this->head[$k])) $this->sheet->write($count+1, $this->head[$k], $v);
-			else user_error("Key '{$k}' not found in headers names");
+            if (isset($this->head[$k])) $this->sheet->write($count+1, $this->head[$k], $v);
+            else user_error("Key '{$k}' not found in headers names");
         }
     }
 }
