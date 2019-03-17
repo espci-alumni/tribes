1c73170f7728374bad7ba0e566ee87fb87fac858
espci.org <postmaster@espci.org>
2019-03-17 15:49:10 +0100

    svp

--- cotisation/class/agent/cotiser/merci.php
+++ cotisation/class/agent/cotiser/merci.php
@@ -5,4 +5,18 @@ class agent_cotiser_merci extends agent_tpe_callback
     const contentType = 'text/html';
 
     protected $template = 'cotiser/merci';
+
+    function control()
+    {
+        if ('GET' === $_SERVER['REQUEST_METHOD'] && SESSION::get('cotisation_contact_id'))
+        {
+            SESSION::set(array(
+                'cotisation_contact_id' => '',
+                'cotisation_email' => '',
+                'cotisation_next_step' => '',
+            ));
+        }
+
+        parent::control();
+    }
 }
